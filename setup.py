9542fd5bfef4d1734d034b5e495e839fac302be0
Kumar, Vipin <vipin.v.kumar@barco.com>
2021-06-19 02:24:24 +0530

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ setup(
 name = "TimeConverter",
 version = "0.0.1",
 author = "Vipin Kumar",
+author_email = "k.vipz88@gmail.com"
+description = "A template for Python package"
 
 package_dir = {"": "src"},
 packages = ["package_main"],
+
+# packages = find_packages(
+#    where = "src"
+#)
+
 entry_points = {
     'console_scripts': [
         'tc=package_main.cli:tc'
